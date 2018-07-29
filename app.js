0d9477f0b4bf6f5562cebc70c057457b1844503a
Titanium <xingchenlyx@qq.com>
2018-07-29 22:39:30 +0800

    注册模块完成

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 require('./database/config');
 
 
@@ -22,6 +24,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({  //这是将session 保存在内存当中   每个人分配一个session  但是访问量过多，内存肯定是扛不住的，所以必须做 ‘持久化处理’(用connect-mongo 插件)
+    secret: 'LC',   //签名秘钥  加密的过程中需要  可以自己定
+    resave: false,   //是否允许session重新修改
+    saveUninitialized: true,   //是否保存初始化的session
+    cookie: { secure: false }, //不能用默认的，改为false ,因为会没有效果，它走的是https ,我们没有
+    store: new MongoStore({    //options即（）里一般写配置项
+        url: 'mongodb://localhost/note',
+        ttl: 14 * 24 * 60 * 60  //过期时间: 设的是14天
+    })  //这便为session持久化
+}))
 
 
 app.use('/',index);
--- controller/register.js
+++ controller/register.js
@@ -1,22 +1,30 @@
 const {Router} = require ("express");
 const router = Router();
 const user = require('../database/user');
+const isEmail = require('validator/lib/isEmail'); // 一种很牛的关于验证的插件，可以验证很多东西
 
 router.post('/user',(req,res) => {
     let {username, email, password} = req.body;
-    console.log(username, email, password);
-    user.create({username,password,email}).then(data => {
-        res.json({
-            code: 200,
-            msg: "success"
+
+    if(isEmail(email)&&password&&password.trim()!= ''){
+        user.create({username,password,email}).then(data => {
+            res.json({
+                code: 200,
+                msg: "success"
+            })
+        }).catch(err => {
+            // console.log(err);
+            res.json({
+                code: 401,
+                msg: "该邮箱已注册"
+            })
         })
-    }).catch(err => {
-        // console.log(err);
+    }else{
         res.json({
             code: 401,
-            msg: "该邮箱已注册"
+            msg: "注册信息错误，请重新输入"
         })
-    })
+    }
 });
 
 module.exports = router;
