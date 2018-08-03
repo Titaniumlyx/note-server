9719886bf815c72233023ec967cbadec7dc057c5
Titaniumlyx <xingchenlyx@qq.com>
2018-08-03 12:31:31 +0800

    模糊搜索

--- controller/article.js
+++ controller/article.js
@@ -1,7 +1,7 @@
 const {Router} = require ("express");
 const router = Router();
 const article = require('../database/model/article');
-
+// 增加文章
 router.post('/article', (req,res) => {
     if(req.session.user){
         console.log(req.session.user);
@@ -27,9 +27,11 @@ router.post('/article', (req,res) => {
         })
     }
 });
-
+// 得到文章列表
 router.get('/getArticle',(req,res) => {
-    article.find().then(data => {
+    article.find()
+        .sort({_id: -1})
+        .then(data => {
         res.json({
             code: 200,
             data,
@@ -42,7 +44,7 @@ router.get('/getArticle',(req,res) => {
         })
     })
 });
-
+// 进入文章详情
 router.post('/getArticle/:id', (req,res) => {
     let {id} = req.params;
     article.findOne({_id: id}).then(data => {
@@ -52,6 +54,32 @@ router.post('/getArticle/:id', (req,res) => {
             msg: 'success'
         })
     })
+});
+// 模糊搜索
+router.get('/articleSearch', (req,res) => {
+   let {title, pn=1, size=10} = req.query;
+   let query = {};
+   if(title){
+       let reg = new RegExp(title);  //reg 便是 title 即要搜索的对象
+       // 用‘正则表达式’来查找搜索
+       query = {
+           $or : [
+               {title: reg},
+               {content: reg}
+           ]
+       }
+   }
+   article.find(query)
+       .sort({_id: -1}) //倒序排列
+       .skip(size*(pn-1)) //跳过多少页
+       .limit(size)  //限制多少页
+       .then(data => {
+       res.json({
+           code: 200,
+           data,
+           msg: 'success'
+       })
+   })
 });
 
 module.exports = router;
--- controller/index.js
+++ controller/index.js
@@ -5,6 +5,13 @@ const register = require('./register');
 const login = require('./login');
 const swiper = require('./swiper');
 const article = require('./article');
+
+const path = require('path');
+
+router.get('/', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '../pages/index.html'));
+});
+
 router.use(register);
 router.use(login);
 router.use(swiper);
