0e1e9bfbe4d3be1a05dcf85d2112f9473e1842ec
Titaniumlyx <xingchenlyx@qq.com>
2018-08-03 19:02:57 +0800

    改数据库

--- database/config.js
+++ database/config.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/lyx-note',{ useNewUrlParser: true});
 
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
